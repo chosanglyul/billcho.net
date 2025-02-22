fe1c289bcb00f484e3d160753b93e6de96b04b98
SangLyul Cho <chosanglyul@gmail.com>
2025-02-22 14:46:03 +0900

    Main page works!

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,44 @@
-import Link from 'next/link';
-import profileData from '@/public/profile.json';
+'use client';
 
+import Link from 'next/link';
+import React, { useEffect, useRef } from 'react';
+
+import profileData from '@/public/profile.json';
+import styles from '@/styles/main.module.css';
+
+const name: string = profileData.name;
 const about: string = profileData.about;
+const abstract: string = "My primary interest lies in machine learning (ML) and the acceleration of large-scale deep learning (DL) models. I have a diverse background in ML, systems, competitive programming, and web development. Having observed the escalating growth and complexity of DL models, I've developed an interest in tackling the challenges of efficient training and serving these models. To deepen my understanding and explore these interests further, I am double majoring in math.";
 
 const Main: React.FC = () => {
   return (
-    <main className="flex items-center justify-center max-w-4xl h-screen mx-auto p-4">
-      <div>
-        <p className="text-lg font-semibold text-center">{about}</p>
-        <p>
-        My primary interest lies in machine learning (ML) and the acceleration of large-scale deep learning (DL) models. I have a diverse background in ML, systems, competitive programming, and web development. Having observed the escalating growth and complexity of DL models, I've developed an interest in tackling the challenges of efficient training and serving these models. To deepen my understanding and explore these interests further, I am double majoring in math.
-        </p>
-        <div className="flex justify-center space-x-4 mt-4">
-          <Link href="/about">
-            <button className="flex items-center px-4 py-2 bg-blue-500 text-white rounded-md">
-              About
-              <span className="ml-2">&#8594;</span>
-            </button>
-          </Link>
-          <Link href="/publications">
-            <button className="flex items-center px-4 py-2 bg-blue-500 text-white rounded-md">
-              Publications
-              <span className="ml-2">&#8594;</span>
-            </button>
-          </Link>
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <div className={styles.bar}></div>
+        <div className={`${styles.arrow} ${styles.lRect}`}>
+          <p className={styles.arrowTitleText}>Machine Learning</p>
+        </div>
+        <div className={`${styles.arrow} ${styles.rRect}`}>
+          <p className={styles.arrowTitleText}>Computer Systems</p>
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.title}>{name}</h1>
+          <p className={styles.about}>{about}</p>
+          <p className={styles.abstract}>{abstract}</p>
+          <div className={styles.buttonContainer}>
+            <Link href="/about">
+              <button className={styles.button}>
+                About
+                <span className="ml-2">&#8594;</span>
+              </button>
+            </Link>
+            <Link href="/publications">
+              <button className={styles.button}>
+                Publications
+                <span className="ml-2">&#8594;</span>
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
     </main>
